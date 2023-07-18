e98b9652d33b61a94fd6943f99581140cc53fae1
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-07-18 16:20:53 +0800

    finished 1-15

--- lesson1/index.js
+++ lesson1/index.js
@@ -1,5 +1,144 @@
-let number=1
-const leftBorderWith=1
+'use strict';
+//lesson 15
+console.log('arr' + '-object');
+console.log(4 + '-object');
+console.log(4 + '4');
 
-number=5
-console.log(number);
+let incr = 10,
+    decr = 10;
+incr++;
+decr++;
+console.log(incr);
+console.log(decr);
+console.log(5 % 2);
+console.log(2 * 4 === 8);
+console.log(2 * 4 === '8');
+
+const isChecked = true,
+    isClose = false;
+console.log(isChecked || isClose);
+
+
+// //lesson 14
+// const category="toys"
+// console.log("https://someurl.com/"+category)
+// console.log(`https://someurl.com/${category}`)
+// const user="Ivan"
+// alert(`Hello, ${user}`)
+
+// //lesson 13
+// const storeName="Five"
+// const storeDescription={
+//     budget:10000,
+//     employees:["John","John","John"],
+//     products:{
+//         milk:100,
+//         bread:50
+//     },
+//     open:true
+// }
+
+// alert("Hello")
+
+// const result=confirm("Are you here")
+// console.log(result);
+
+// const answer=prompt("Вам есть 18?")
+// console.log(typeof (answer));
+
+// const answers = [];
+// // answers[0]=prompt("What is your name?")
+// // answers[1]=prompt("What is your lastname?")
+// // answers[2]=prompt("How old are you? ")
+// console.log(typeof (answers));
+
+// document.write(answers)
+// // // Lesson 12
+// const arr = [1, 2, 3, 4];
+// const arrObj = {
+//     0: 1,
+//     1: 2,
+//     2: 3,
+//     3: 4,
+//     abc: {
+//         a: 'a',
+//         b: {b: 'b'},
+//         c:["c","c"]
+//     }
+// };
+//
+// arrObj.b = '154';
+//
+// console.log(arr['1']);
+// console.log(arrObj['1']);
+// console.log(arrObj['b']);
+// console.log(arrObj.b);
+//
+// const obj = {Anna: 500, Alice: 800};
+// console.log(obj['Anna']);
+
+// const obj = {a: 1, b: 2};
+// console.log(obj["a"]);
+
+// // Lesson 11
+// let number = 4.6;
+// console.log(-4 / 0);
+// console.log('string' * 9);
+//
+
+//
+// const bool=true
+// //
+// // console.log(something)
+//
+// let ind
+// console.log(ind);
+//
+//
+// const obj={
+//     name:"John",
+//     age:25,
+//     isMarried:false
+// }
+// console.log(obj["name"])
+// console.log(obj.name)
+//
+// let arr=["plum.png","orange.jpg","apple.bmp",6,{},[]]
+//
+// console.log(arr["0"]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(arr[4]);
+// console.log(arr[5]);
+// console.log(arr[6]);
+
+//Lesson 10
+// const userName="John"
+// let userNumber=25
+// userNumber=24
+//
+//Lesson 11
+
+// const a = 5000
+// const b = 4000
+//
+//
+//
+//
+// // let number = 1;
+// const leftBorderWith = 1;
+//
+// number = 5;
+// console.log(number);
+//
+//
+// {
+//     let result = 50;
+// }
+// console.log(result);
+//
+// {
+//     let result = 50;
+// }
+// console.log(result);
