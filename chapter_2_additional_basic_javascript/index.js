25e8f28846ce22b19dac2e2711faf801dcb27682
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-10-24 23:02:08 +0800

    started new chapter#3, finished lesson 63

--- chapter_2_additional_basic_javascript/index.js
+++ chapter_2_additional_basic_javascript/index.js
@@ -1,30 +1,30 @@
-// //lesson 61 Bigint
-
-// console.log(Number.MAX_SAFE_INTEGER);
-//(2^53)-1
-
-// const bigint = 1111111111111111115888888888n;
+// // //lesson 61 Bigint
 //
-// const sameBigint = BigInt(5111111115454848778787987987987798);
-// console.log(typeof bigint);
-// console.log(typeof sameBigint);
-
-console.log(2==2n)
-console.log(2===2n)
-
-
-let bigint=1n
-let number=2
-console.log(bigint+BigInt(number));
-// console.log(+bigint+BigInt(number));
-console.log(Number(bigint+BigInt(number)));
-// console.log(5n>2n)
-// console.log(5n>2)
-// console.log(5n<2)
-// console.log(Math.floor(5n));
-// console.log(5n/2n) //2n
-// console.log(5n+1n);
-// console.log(5n+1);
+// // console.log(Number.MAX_SAFE_INTEGER);
+// //(2^53)-1
+//
+// // const bigint = 1111111111111111115888888888n;
+// //
+// // const sameBigint = BigInt(5111111115454848778787987987987798);
+// // console.log(typeof bigint);
+// // console.log(typeof sameBigint);
+//
+// console.log(2==2n)
+// console.log(2===2n)
+//
+//
+// let bigint=1n
+// let number=2
+// console.log(bigint+BigInt(number));
+// // console.log(+bigint+BigInt(number));
+// console.log(Number(bigint+BigInt(number)));
+// // console.log(5n>2n)
+// // console.log(5n>2)
+// // console.log(5n<2)
+// // console.log(Math.floor(5n));
+// // console.log(5n/2n) //2n
+// // console.log(5n+1n);
+// // console.log(5n+1);
 // console.log(5n+"1");
 
 // //lesson 60 Set
