fb3219742de1930d11e082e3e8ecb32129f72bd0
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-10-24 21:14:08 +0800

    finished lesson about Map #59

--- chapter_2_additional_basic_javascript/index.js
+++ chapter_2_additional_basic_javascript/index.js
@@ -1,38 +1,97 @@
-//lesson 58
-const user = {
-    name: 'Alex',
-    surname: 'Smith',
-    birthday: '20/04/1993',
-    showMyPublicData() {
-        console.log(`${this.name} ${this.surname}`);
-    }
-};
-// for (const key in user) {
-//     console.log(key);
+//lesson 59 Map
+//
+//
+// const user = {
+//     name: 'Alex',
+//     surname: 'Smith',
+//     birthday: '20/04/1993',
+//     showMyPublicData() {
+//         console.log(`${this.name} ${this.surname}`);
+//     }
+// };
+// // console.log(user);
+// // console.log(typeof (Object.keys(user)[0]));
+//
+//
+// const shops = [
+//     {rice: 500},
+//     {oil: 200},
+//     {bred: 50},
+// ];
+// const budget = [5000, 15000, 25000];
+//
+// const map = new Map([
+//     [{paper: 544}, 800]]);
+// shops.forEach((shop, i) => {
+//     map.set(shop, budget[i]);
+// });
+// const goods = [];
+//
+// for (let shop of map.keys()) {
+//     goods.push(Object.keys(shop)[0]);
 // }
-const arr = ['b', 'a', 'c'];
-
-Array.prototype.someMethod = function () {
-};
-console.log(arr);
-for (const el in arr) {
-    console.log(el);
-}
-const salaries = {
-    john: 500,
-    ivan: 1000,
-    ann: 5000,
-    sayHello: function () {
-        console.log('Hello');
-    }
-};
-salaries[Symbol.iterator] = function () {
-    return {
-        next() {
-            // {done:true, value:123}
-        }
-    };
-};
+// map.forEach((value, key, map) => {
+//     console.log(key, value);
+// });
+//
+// const userMap = new Map(Object.entries(user));
+// console.log(userMap);
+// const newUserObj = Object.fromEntries(userMap);
+// console.log(newUserObj);
+// // console.log(goods);
+//
+// for (let [shop, price] of map.entries()){
+//     console.log(shop, price);
+// }
+// console.log(map.get(shops[0]));
+// console.log(map.has(shops[0]));
+// console.log(map.delete(shops[0]));
+// console.log(map.clear());
+// console.log(map.get(shops[0]));
+//map.keys()
+//
+// console.log(shops);
+// console.log(map);
+// //
+// map
+//     .set(shops[0], 5000)
+//     .set(shops[1], 15000)
+//     .set(shops[2], 25000);
+//lesson 58
+// const user = {
+//     name: 'Alex',
+//     surname: 'Smith',
+//     birthday: '20/04/1993',
+//     showMyPublicData() {
+//         console.log(`${this.name} ${this.surname}`);
+//     }
+// };
+// // for (const key in user) {
+// //     console.log(key);
+// // }
+// const arr = ['b', 'a', 'c'];
+//
+// Array.prototype.someMethod = function () {
+// };
+// console.log(arr);
+// for (const el in arr) {
+//     console.log(el);
+// }
+// const salaries = {
+//     john: 500,
+//     ivan: 1000,
+//     ann: 5000,
+//     sayHello: function () {
+//         console.log('Hello');
+//     }
+// };
+// salaries[Symbol.iterator] = function () {
+//     return {
+//         next() {
+//             // {done:true, value:123}
+//         }
+//     };
+// };
 
 // const arr = ['b', 'a', 'c'];
 // for (const key in arr){
