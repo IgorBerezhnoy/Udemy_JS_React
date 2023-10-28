b70d498617aedba21be22e4c40635ee271813537
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-10-28 21:38:49 +0800

    finished lessons #66-67

--- chapter_3_javascript_in_work/less#63/index.js
+++ chapter_3_javascript_in_work/less#63/index.js
@@ -1,39 +1,39 @@
-const btns = document.querySelectorAll('button');
-const wrapper = document.querySelector('.btn-block');
-// console.log(btns);
-// console.log(btns[0].classList.length);
-// console.log(btns[0].classList.item(0));
-// console.log(btns[0].classList.item(1));
-// btns[1].classList.add('red');
-// btns[0].classList.remove('blue');
-// console.log(btns[0].classList.toggle("blue"));
+// const btns = document.querySelectorAll('button');
+// const wrapper = document.querySelector('.btn-block');
+// // console.log(btns);
+// // console.log(btns[0].classList.length);
+// // console.log(btns[0].classList.item(0));
+// // console.log(btns[0].classList.item(1));
+// // btns[1].classList.add('red');
+// // btns[0].classList.remove('blue');
+// // console.log(btns[0].classList.toggle("blue"));
+// //
+// // if (btns[1].classList.contains("red")){
+// //     console.log("red");
+// // }
+// console.log(btns[0]);
+// // console.log(btns[0].className);
+// btns[0].addEventListener('click', () => {
+//     // if(!btns[1].classList.contains("red")){
+//     btns[1].classList.toggle('red');
+//     // btns[1].classList.add("red")
+//     // }else {
+//     //     btns[1].classList.remove('red')
+//     // }
+// });
+// //
+// // btns.forEach(btn=>{
+// //     btn.addEventListener("click", ()=>{
+// //         console.log("hell");
+// //     })
+// // })
+// wrapper.addEventListener('click', (e) => {
+//     if (e.target && e.target.matches("button.red")) {
+//     // if (e.target && e.target.tagName === 'BUTTON') {
+//         console.log('hello');
+//     }
+// });
 //
-// if (btns[1].classList.contains("red")){
-//     console.log("red");
-// }
-console.log(btns[0]);
-// console.log(btns[0].className);
-btns[0].addEventListener('click', () => {
-    // if(!btns[1].classList.contains("red")){
-    btns[1].classList.toggle('red');
-    // btns[1].classList.add("red")
-    // }else {
-    //     btns[1].classList.remove('red')
-    // }
-});
-//
-// btns.forEach(btn=>{
-//     btn.addEventListener("click", ()=>{
-//         console.log("hell");
-//     })
-// })
-wrapper.addEventListener('click', (e) => {
-    if (e.target && e.target.matches("button.red")) {
-    // if (e.target && e.target.tagName === 'BUTTON') {
-        console.log('hello');
-    }
-});
-
-const btn = document.createElement('button');
-btn.classList.add('red');
-wrapper.append(btn);
+// const btn = document.createElement('button');
+// btn.classList.add('red');
+// wrapper.append(btn);
---
+++ chapter_3_javascript_in_work/less#65_67/index.js
@@ -0,0 +1,130 @@
+//lesson67
+
+
+//
+let cache = new WeakMap();
+
+function cacheUser(user) {
+    if (!cache.has(user)) {
+        cache.set(user, Date.now());
+    }
+    return cache.get(user);
+}
+
+let lena = {name: 'Elena'};
+let alex = {name: 'Alex'};
+cacheUser(lena);
+cacheUser(alex);
+
+lena = null;
+console.log(cache.has(lena));
+console.log(cache.has(alex));
+
+let messages = [
+    {text: 'Hello', from: 'John'},
+    {text: 'World', from: 'Alex'},
+    {text: '.....', from: 'M'}
+];
+
+let readMessages = new WeakSet();
+readMessages.add(messages[0]);
+readMessages.add(messages[1]);
+
+console.log(readMessages.has(messages[0]));
+//
+// let user = {name: 'Ivan'};
+//
+// // const arr=[user]
+// let map = new WeakMap();
+// map.set(user, 'data');
+//
+// user = null;
+//
+// console.log(user);
+// console.log(map);
+// console.log(map);
+// console.log(map.keys());
+// console.log(arr[0])
+// console.log(arr)
+
+
+//lesson66
+// function func() {
+//     window.smth = 'string';
+// }
+
+//
+// const someRes=getData()
+// const node=document.querySelector(.class)
+// setInterval(function (){
+//     if (node){
+//         node.innerHTML=someRes
+//     }
+// },1000)
+
+// function outer() {
+//     const potentiallyHugeArray = [];
+//
+//     return function inner() {
+//         potentiallyHugeArray.push('Hello');
+//         console.log("hello");
+//     };
+// }
+// const sayHello=outer()
+
+
+// function createElement() {
+//     const div = document.createElement('div');
+//     div.id = 'test';
+//     return div;
+// }
+//
+// const tesDiv = createElement();
+// document.body.append(tesDiv);
+//
+// function deleteElement() {
+//     document.body.removeChild(document.getElementById('test'));
+// }
+// deleteElement()
+
+// const btn = document.querySelector('.btn');
+// let timerId;
+// let i = 0;
+// btn.addEventListener("click", ()=>{
+//     myAnimation()
+// })
+// function myAnimation(){
+//     const elem=document.querySelector(".box")
+//     let pos=0
+//     let id=setInterval(frame, 10)
+//
+//     function frame(){
+//         if (pos===300){
+//             clearInterval(id)
+//         }else {
+//             pos++
+//             elem.style.top=pos+"px"
+//             elem.style.left=pos+"px"
+//         }
+//     }
+// }
+//
+// // btn.addEventListener('click', () => {
+// //     timerId = setInterval(logger, 1000);
+// // });
+// //
+// //
+// // function logger() {
+// //     if (i > 3) {
+// //         clearInterval(timerId);
+// //         console.log('Таймер удален');
+// //
+// //     }
+// //     console.log('text');
+// //     i++;
+// // }
+// //
+// // const id = setTimeout(function log() {
+// //     console.log('hello');
+// //     id = setTimeout(log, 1000);
+// // });
