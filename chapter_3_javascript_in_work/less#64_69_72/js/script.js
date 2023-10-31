e82f2bb40d0ee61313d6dee8dd671991c8596b3d
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-10-31 18:15:14 +0800

    finished lessons #71-72

--- chapter_3_javascript_in_work/less#64_69_72/js/script.js
+++ chapter_3_javascript_in_work/less#64_69_72/js/script.js
@@ -1,6 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
     const tabs = document.querySelectorAll('.tabheader__item');
     const tabsContent = document.querySelectorAll('.tabcontent');
+
     const tabsParent = document.querySelector('.tabheader__items');
 
     function hideTabContent() {
@@ -32,7 +33,7 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
-
+////////////////////////////////////////////////////////
     const deadLine = '2024-05-11';
 
     function getTimeRemaining(endTime) {
@@ -78,43 +79,33 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     setClock('.timer', deadLine);
+    //////////////////////////////////////////////////
+
+    const modalTrigger = document.querySelectorAll('.data-modal');
+    const modal = document.querySelector('.modal');
+    const modalCloseBtn = document.querySelector('[data-close]');
+    const closeModal = () => {
+        modal.style.display = 'none';
+        document.body.style.overflow = '';
+    };
+    modal.classList.add('hide');
+    modalTrigger.forEach(el => {
+
+        el.addEventListener('click', () => {
+            console.dir(modal.classList);
+            modal.style.display = 'block';
+        });
+    });
+    modalCloseBtn.addEventListener('click', closeModal);
+    // data-close
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal();
+        }
+    });
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && modal.style.display === 'block') {
+            closeModal();
+        }
+    });
 });
-//
-// window.addEventListener('DOMContentLoaded', () => {
-//     const tabs = document.querySelectorAll('.tabheader__item');
-//     const tabsContent = document.querySelectorAll('.tabcontent');
-//     const tabsParent = document.querySelector('.tabheader__items');
-//
-//
-//     function hideTabContent() {
-//         tabsContent.forEach(el => {
-//             el.classList.add("hide")
-//             el.classList.remove("show", "fade")
-//         });
-//         tabs.forEach(el => {
-//             el.classList.remove('tabheader__item_active');
-//         });
-//     }
-//
-//     function showTabContent(i = 0) {
-//         tabsContent[i].classList.add("show", "fade")
-//         tabsContent[i].classList.remove("hide")
-//         tabs[i].classList.add('tabheader__item_active');
-//
-//     }
-//
-//     hideTabContent();
-//     showTabContent();
-//     tabsParent.addEventListener('click', (e) => {
-//         const target=e.target
-//         if (target&&target.classList.contains("tabheader__item")){
-//             tabs.forEach((el,i)=>{
-//                 if (el===target){
-//                     hideTabContent();
-//                     showTabContent(i);
-//                 }
-//             })
-//         }
-//     });
-//
-// });
