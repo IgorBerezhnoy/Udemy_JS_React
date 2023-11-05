ed434d7187675a25a86a398efae1e57ff5ceecd7
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2023-11-05 16:10:54 +0800

    started new chapter finished lessons 80-81

---
+++ chapter_4_advans_javascript_/allLessons.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+
+// //82. JSON формат передачи данных, глубокое клонирование объектов
+// const persone = {
+//   name: 'Alex',
+//   tel: '+744444444444444',
+//   parents:{
+//     mom:"Olga",
+//     dad:"Mike"
+//   }
+// };
+// console.log(JSON.parse(JSON.stringify(persone)));
