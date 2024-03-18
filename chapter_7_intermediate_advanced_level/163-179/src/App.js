e0dd93a727e1712c3f5e2914c7e77d4a3cedfd1c
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2024-03-18 22:37:59 +0800

    finished lessons #179-181

--- chapter_7_intermediate_advanced_level/163-179/src/App.js
+++ chapter_7_intermediate_advanced_level/163-179/src/App.js
@@ -1,35 +1,238 @@
-import React, {useMemo, useState} from 'react';
+import {useEffect, useState} from 'react';
+import {Container} from 'react-bootstrap';
 import './App.css';
-import {Form} from './Form';
-import {dataContext} from './DataContext';
 
+const getDataFromFirstFetch = () => {
+  return 10;
+};
+const getDataFromSecondFetch = () => {
+  return 20;
+};
 
-function App() {
-  const {Provider, Consumer} = dataContext;
-  const [data, setData] = useState({
-    mail: 'first@example.com',
-    text: 'some text',
-    count: 0,
-    forceChangeMail
-  });
+function WithSlider(BaseComponent, getData) {
+  return function (props) {
+    const [slide, setSlide] = useState(0);
+    const [autoplay, setAutoplay] = useState(false);
 
-  function forceChangeMail() {
-    setData((data) => ({...data, mail: 'name@examp1111le.com'}));
-  }
+    useEffect(() => {
+      setSlide(getData());
+    }, []);
+
+    function changeSlide(i) {
+      setSlide(slide => slide + i);
+    }
+
+    return <BaseComponent
+      {...props}
+      slide={slide}
+      changeSlide={changeSlide}
+      setAutoplay={setAutoplay}
+      autoplay={autoplay}/>;
+  };
+};
+
+const SliderFirst = ({slide, changeSlide}) => {
 
   return (
-    <Provider value={data}>
-      <Form text={data.text} count={data.count}/>
-      <button
-        onClick={() => setData({
-          mail: 'name@example.com',
-          text: 'another text',
-          count: data.count++
-        })}>
-        Click me
-      </button>
-    </Provider>
+    <Container>
+      <div className="slider w-50 m-auto">
+        <img className="d-block w-100"
+             src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"
+             alt="slide"/>
+        <div className="text-center mt-5">Active slide {slide}</div>
+        <div className="buttons mt-3">
+          <button
+            className="btn btn-primary me-2"
+            onClick={() => changeSlide(-1)}>-1
+          </button>
+          <button
+            className="btn btn-primary me-2"
+            onClick={() => changeSlide(1)}>+1
+          </button>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+const SliderSecond = ({slide, changeSlide, autoplay, setAutoplay}) => {
+  return (
+    <Container>
+      <div className="slider w-50 m-auto">
+        <img className="d-block w-100"
+             src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"
+             alt="slide"/>
+        <div className="text-center mt-5">Active slide {slide} <br/>{autoplay ? 'auto' : null} </div>
+        <div className="buttons mt-3">
+          <button
+            className="btn btn-primary me-2"
+            onClick={() => changeSlide(-1)}>-1
+          </button>
+          <button
+            className="btn btn-primary me-2"
+            onClick={() => changeSlide(1)}>+1
+          </button>
+          <button
+            className="btn btn-primary me-2"
+            onClick={() => setAutoplay(autoplay => !autoplay)}>toggle autoplay
+          </button>
+        </div>
+      </div>
+    </Container>
+  );
+};
+const WithSliderFirst = WithSlider(SliderFirst, getDataFromFirstFetch);
+const WithSliderSecond = WithSlider(SliderSecond, getDataFromSecondFetch);
+
+
+const WithLogger = (WrapperComponent) => (props) => {
+  console.log('Hello');
+  return <WrapperComponent {...props} />;
+};
+const Hello = () => {
+  return <h1>Hello</h1>;
+};
+const WithLoggerHello = WithLogger(Hello);
+function App() {
+  return (
+    <>
+      <WithLoggerHello/>
+      <WithSliderFirst/>
+      <WithSliderSecond/>
+    </>
   );
 }
 
 export default App;
+
+
+// import {useReducer, useState} from 'react';
+// import {Container} from 'react-bootstrap';
+// import './App.css';
+//
+// function reducer(state, action) {
+//   switch (action.type) {
+//     case 'toggle': {
+//       return {
+//         ...state,
+//         autoplay: !state.autoplay,
+//       };
+//     }
+//     case 'slow': {
+//       return {
+//         ...state,
+//         autoplay: 300,
+//       };
+//     }
+//     case 'fast': {
+//       return {
+//         ...state,
+//         autoplay: 700,
+//       };
+//
+//     }
+//     case '500': {
+//       return {
+//         ...state, autoplay: action.speed
+//       };
+//     }
+//     default:
+//       throw new Error('unknown action');
+//   }
+// }
+//
+// const Slider = () => {
+//   const [slide, setSlide] = useState(0);
+//   // const [autoplay, setAutoplay] = useState(false);
+//   const [autoplay, dispatch] = useReducer(reducer, {autoplay: false});
+//   console.log(autoplay);
+//
+//   function changeSlide(i) {
+//     setSlide(slide => slide + i);
+//   }
+//
+//   return (
+//     <Container>
+//       <div className="slider w-50 m-auto">
+//         <img className="d-block w-100"
+//              src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg"
+//              alt="slide"/>
+//         <div className="text-center mt-5">Active slide {slide} <br/>{autoplay.autoplay ? 'auto' : null} </div>
+//         <div className="buttons mt-3">
+//           <button
+//             className="btn btn-primary me-2"
+//             onClick={() => changeSlide(-1)}>-1
+//           </button>
+//           <button
+//             className="btn btn-primary me-2"
+//             onClick={() => changeSlide(1)}>+1
+//           </button>
+//
+//           <button
+//             className="btn btn-primary me-2"
+//             onClick={() => dispatch({type: 'toggle'})}>toggle autoplay
+//           </button>
+//           <button
+//             className="btn btn-primary me-2"
+//             onClick={() => dispatch({type: 'slow'})}>slow autoplay
+//           </button>
+//           <button
+//             className="btn btn-primary me-2"
+//             onClick={() => dispatch({type: 'fast'})}>fast autoplay
+//           </button>
+//           <button
+//             className="btn btn-primary me-2"
+//             onClick={() => dispatch({type: '500', speed: 500})}>500 autoplay
+//           </button>
+//
+//         </div>
+//       </div>
+//     </Container>
+//   );
+// };
+//
+// function App() {
+//   return (
+//     <Slider/>
+//   );
+// }
+//
+// export default App;
+
+
+// import React, {useMemo, useState} from 'react';
+// import './App.css';
+// import {Form} from './Form';
+// import {dataContext} from './DataContext';
+//
+//
+// function App() {
+//   const {Provider, Consumer} = dataContext;
+//   const [data, setData] = useState({
+//     mail: 'first@example.com',
+//     text: 'some text',
+//     count: 0,
+//     forceChangeMail
+//   });
+//
+//   function forceChangeMail() {
+//     setData((data) => ({...data, mail: 'name@examp1111le.com'}));
+//   }
+//
+//   return (
+//     <Provider value={data}>
+//       <Form text={data.text} count={data.count}
+//       />
+//       <button
+//         onClick={() => setData({
+//           mail: 'name@example.com',
+//           text: 'another text',
+//           count: data.count++
+//         })}>
+//         Click me
+//       </button>
+//     </Provider>
+//   );
+// }
+//
+// export default App;
--- chapter_7_intermediate_advanced_level/163-179/src/DataContext.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const dataContext = React.createContext({
-  mail: 'first@example.com',
-  text: 'some text',
-  count: 0
-});
