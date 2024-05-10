27c12112698ca739434ed51e18a53726c2e65763
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2024-05-10 16:24:58 +0800

    Less-197 [feat]: work with Redux

--- chapter_8_Redux/190-192/my-app/src/actions.js
+++
@@ -1,5 +0,0 @@
-export let inc = () => ({type: 'INC'});
-export let dec = () => ({type: 'DEC'});
-export let rand = () => ({type: 'RAND', payload: Math.floor(Math.random() * 10)});
-
-export const actions = {inc, dec, rand};
