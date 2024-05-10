1a5ba93ea1db64d75dacfb749256702bf5fa234a
IgorBerezhnoy <i-berezhnoy@inbox.ru>
2024-05-11 00:58:02 +0800

    Less-201-202 [feat]: added custom middleWare

--- chapter_8_Redux/197/src/store/index.js
+++ chapter_8_Redux/197/src/store/index.js
@@ -1,12 +1,26 @@
-import {createStore} from 'redux';
-import {combineReducers} from '@reduxjs/toolkit';
+import {createStore, combineReducers, compose, applyMiddleware} from 'redux';
+import {thunk} from 'redux-thunk';
 import {heroes} from '../reducers/heroes';
 import {filters} from '../reducers/filters';
 
+const stringMiddleware = ({dispatch, getState}) => (next) => (action) => {
+  if (typeof action === 'string') {
+    return next({type: action});
+  }
+  return next(action);
+};
+
 
 const store = createStore(combineReducers({
-  heroes,
-  filters
-}), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
+      heroes,
+      filters
+    }),
+    compose(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(thunk, stringMiddleware))
+// applyMiddleware(stringMiddleware)
+// , compose(enhancer,
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// )
+  )
+;
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 export default store;
